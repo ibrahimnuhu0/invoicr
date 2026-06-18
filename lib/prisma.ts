4191d820f8064e40adb00428c42b3272338c3588
Ibrahim Nuhu <ibrahimnuhu0@gmail.com>
2026-06-18 23:47:19 +0100

    fix: use require for PrismaClient to resolve Prisma 7 export error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require("@prisma/client");
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: any | undefined;
 };
 
 function makePrisma() {
